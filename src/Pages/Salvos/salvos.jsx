bd5f905e2a704f6d37fa95eade8b815f282709ae
Wickedju <almeida_2010@outlook.com>
2024-09-04 11:34:02 -0300

    telas

---
+++ src/Pages/Salvos/salvos.jsx
@@ -0,0 +1,16 @@
+import SideBar from '../../Components/SideBar/SideBar'
+import './salvos.css'
+
+function salvos(){
+
+    return(
+        <div className='pai bg'>
+              <div className='title'>
+                <h2 className='colecoes'>Coleções</h2>
+              </div>
+              <SideBar />
+              </div>
+    )
+}
+
+export default salvos
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import App from "./App";
 import "./index.css";
 
 // importar as paginas
-import Principal from "./Pages/Principal/Principal";
 import TelaLogin from "./Pages/telaLogin/TelaLogin";
+import Salvos from "./Pages/Salvos/salvos"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="telaLogin" element={<TelaLogin />} />
-          <Route path="/" element={<Principal />} />
+          <Route path="salvos" element={<Salvos/>} />
         </Route>
       </Routes>
     </BrowserRouter>
