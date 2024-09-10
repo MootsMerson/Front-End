ae002ed12ea39e690600010139ec7d5272f2a516
Wickedju <almeida_2010@outlook.com>
2024-09-10 10:35:40 -0300

    telaColecaoQuatro

--- src/Pages/Salvos/salvos.jsx
+++ src/Pages/Salvos/salvos.jsx
@@ -6,10 +6,13 @@ function salvos(){
     return(
 <div className='bg pai'>
       <SideBar />
+      <div className='divMain'>
         <div className='titulo'>
               <div className='title'>
                 <h2 className='colecoes'>Coleções</h2>
-              </div>      
+              </div>  
+        </div>
+        <div className='divNadaSalvo'> <h2 className='nadaSalvo'>Você ainda não possui nada salvo.</h2> </div>
         </div>
         </div>
     )
