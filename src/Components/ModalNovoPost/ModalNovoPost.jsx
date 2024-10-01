0cfe738161a2fed847a122ac6d679d3cf7f1edeb
liviaBELLO <livia_bello@outlook.com>
2024-10-01 11:23:33 -0300

    iniciado

--- src/Components/ModalNovoPost/ModalNovoPost.jsx
+++ src/Components/ModalNovoPost/ModalNovoPost.jsx
@@ -1,10 +1,28 @@
 import "./modalNovoPost.css";
+import perfil from "../../assets/img/imagemPerfil.png";
 
-export default function modalNovoPost(){
-    return(
-        <div className="containerModalNovoPost">
-            
+export default function modalNovoPost() {
+  return (
+    <div className="containerModalNovoPost">
+      <div className="modalNovoPost bg">
+        <div className="tituloModalNovo">
+          <h1>Nova Publicação</h1>
+          <hr className="listraTitulo"></hr>
         </div>
-
-    );
+        <div className="perfilNovoPost">
+          <img src={perfil} alt="perfil"></img>
+          <p>Leonardo</p>
+          <div className="textoNovoPost">
+            <input
+              type="text"
+              placeholder="Escreva o que você está pensando..."
+            ></input>
+          </div>
+        </div>
+        <div className="botaoPublicar">
+          <button>Publicar</button>
+        </div>
+      </div>
+    </div>
+  );
 }
